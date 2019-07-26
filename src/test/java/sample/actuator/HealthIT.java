8da5dcc61a5a105de85af1ad14c2a42d92acacd2
Chris Altamimi <chrisaltamimi@Chriss-MacBook-Pro.local>
2019-07-26 13:12:11 -0400

    build trigger

--- src/test/java/sample/actuator/HealthIT.java
+++ src/test/java/sample/actuator/HealthIT.java
@@ -8,25 +8,25 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class HealthIT {
-	
+
 	@Test
     public void running() {
 		given().when().get("http://localhost:8080/")
             .then().statusCode(200);
     }
-	
+
 	@Test
     public void message() {
 		given().when().get("http://localhost:8080/")
             .then().body(containsString("Spring boot"));
     }
-	
+
 	@Test
     public void fullMessage() {
 		given().when().get("http://localhost:8080/")
-            .then().body("message",equalTo("Spring boot says hello from a Docker container"));
+            .then().body("message",equalTo("A different Spring boot says hello from a Docker container"));
     }
-	
+
 	@Test
     public void health() {
 		given().when().get("http://localhost:8080/actuator/health")
